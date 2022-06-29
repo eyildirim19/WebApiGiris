b50ef52274d75ddd69ccbf759d276c323d8bc459
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-06-29 21:13:45 +0300

    Add project files.

---
+++ Giris/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Giris.Controllers
+{
+    // Route => kaynağın adresi
+    [Route("dersimizapi")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        string[] ogrencilerim = { "Bilal", "Alper", "Emre", "Ahmet" };
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            //return Ok(ogrencilerim);
+            return Ok(ogrencilerim);
+        }
+
+        [HttpGet("{index}")]
+        public IActionResult Get(int index)
+        {
+            try
+            {
+                return Ok(ogrencilerim[index]);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+    }
+}
